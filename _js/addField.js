7df37e54b280657274ff7446a34636c14f38c913
Gabriel Sousa <sousadev.pro@gmail.com>
2020-08-08 16:27:49 -0300

    update addField.js

---
+++ _js/addField.js
@@ -0,0 +1,11 @@
+// Procurar no botão
+document.querySelector("#add-time")
+.addEventListener('click', cloneField)
+// clicar no botão
+
+// ativar uma função
+function cloneField() {
+    //copiar o conteúdo do formulário, que formulário?
+    document.querySelector('.schedule.item')
+    // imprimir na tela
+}

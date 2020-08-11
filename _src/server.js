1169907e2df4b3c12e75c2d43d3eb8c443e5452b
Gabriel Sousa <sousadev.pro@gmail.com>
2020-08-11 11:51:21 -0300

    Update server.js

--- _src/server.js
+++ _src/server.js
@@ -1,6 +1,9 @@
-require('express')()
+const express = require('express')
+const server = express()
+
+server.use(express.static("_public"))
 .get("/", (req, res) => {
-    return res.send("Hello, world!")
+    return res.sendFile(__dirname + "/_views/index.html")
 })
 .get("/study", (req, res) => {
     return res.send("Página study")
